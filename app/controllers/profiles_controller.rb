4a2f0024ab735aabfbc0f6271c1d3d3564852809
John Crodian <johncrodian24@gmail.com>
2017-01-10 17:42:20 -0500

    Created profiles controller and corresponding views. Sign up now redirects to profiles show page.

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,24 @@
+class ProfilesController < ApplicationController
+  def index
+    # @user = User.all
+  end
+
+  def new
+  end
+
+  def show
+    # @user = User.find_by(params[:id])
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'profiles/index'
+
+  get 'profiles/new'
+
+  get 'profiles/show'
+
+  get 'profiles/create'
+
+  get 'profiles/update'
+
+  get 'profiles/edit'
+
+  get 'profiles/destroy'
+
   get 'static_pages/home'
 
   get 'static_pages/about'
@@ -7,35 +21,9 @@ Rails.application.routes.draw do
 
   get 'static_pages/help'
 
-  get 'jobs/index'
-
-  get 'jobs/new'
-
-  get 'jobs/show'
-
-  get 'jobs/create'
-
-  get 'jobs/edit'
-
-  get 'jobs/update'
-
-  get 'jobs/destroy'
-
-  get 'boats/index'
-
-  get 'boats/new'
-
-  get 'boats/show'
-
-  get 'boats/create'
-
-  get 'boats/edit'
-
-  get 'boats/update'
-
-  get 'boats/destroy'
+  resources :jobs, :boats, :profiles
 
   devise_for :views
-  devise_for :users
+  devise_for :users, controllers: { registrations: "registrations" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
