c9b243e98a942c3d49c052ce930053359882dc76
Mia Adjei <adjei.m@gmail.com>
2017-01-11 00:22:59 -0500

    Add CRUD actions and update views for boats. Closes #3

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,22 +1,50 @@
 class BoatsController < ApplicationController
-  def index
-  end
+  before_action :set_boat, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
-  def new
+  def index
+    @boats = Boat.all
   end
 
   def show
   end
 
+  def new
+    @boat = Boat.new
+  end
+
   def create
+    @boat = Boat.new(boat_params)
+    @boat.user_id = current_user.id
+
+    if @boat.save
+      redirect_to @boat
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    @boat.update(boat_params)
+    redirect_to @boat
   end
 
   def destroy
+    @boat.destroy
+    redirect_to boats_path
   end
+
+  private
+
+  def boat_params
+    params.require(:boat).permit(:user_id, :name, :capacity, :location)
+  end
+
+  def set_boat
+    @boat = Boat.find(params[:id])
+  end
+
 end
--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,4 +1,13 @@
 class Boat < ApplicationRecord
 	belongs_to :user
 	has_and_belongs_to_many :jobs
+
+	# def cities
+	# 	attr_accessor :cities
+
+	# 	def initialize
+	# 		@cities = ["Houston", "New York", "Singapore", "Rotterdam", "Hamburg", "Rio de Janeiro", "Lagos", "Shanghai", "Tokyo", "London"]
+	# 	end
+	# end
+
 end
