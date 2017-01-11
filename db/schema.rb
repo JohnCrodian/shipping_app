d0a96225bd8b3fa71a84ef0960052ea98d041dd9
Margaret Wismer <margaretgwismer.gmail.com>
2017-01-11 11:03:27 -0500

    closes #9, closes #5

--- db/schema.rb
+++ db/schema.rb
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20170110203027) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string   "username"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
