752d75924699c1457d1158d24f967ee63808cf54
Mia Adjei <adjei.m@gmail.com>
2017-01-12 16:34:29 -0500

    Begin adding avatar functionality.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,8 +1,11 @@
 class Boat < ApplicationRecord
 	belongs_to :user
 	has_and_belongs_to_many :jobs
+	has_attached_file :avatar
 
 	validates :name, uniqueness: true
+	validates_attachment_content_type :avatar,
+        :content_type => /\Aimage\/.*\Z/
 
 	# def cities
 	# 	attr_accessor :cities
