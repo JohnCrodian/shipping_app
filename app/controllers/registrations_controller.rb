04cf1a99a3e81d10246497d6baccff02eebc482e
Margaret Wismer <margaretgwismer.gmail.com>
2017-01-11 21:46:26 -0500

    closes #16, closes #17, closes #18

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,6 +5,10 @@ class RegistrationsController < Devise::RegistrationsController
    '/profiles/show'
  end
 
+def after_update_path_for(resource)
+    '/profiles/show'
+end
+
  # def after_sign_in_path_for(resource)
  # 	'/profiles/show'
  # end
