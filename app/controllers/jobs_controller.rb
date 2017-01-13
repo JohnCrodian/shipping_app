89a9b3552ba54bc96b4025161effe1a79378e51e
Margaret Wismer <margaretgwismer.gmail.com>
2017-01-13 11:45:38 -0500

    make jobs_controller DRY with add_boats_method

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -18,16 +18,9 @@ class JobsController < ApplicationController
     @job = Job.new(job_params)
     @job.user_id = current_user.id
     @job.cont_present = 0
-    @boats = Boat.all
-    total_cont = 0
-    (1..@boats.length).each do |i|
-     if (params["#{i}"]["id"] == "1")
-       @boat = Boat.find(i)
-       total_cont += @boat.capacity
-       @job.boats.push(@boat)
-     end
-    end
-    @job.cont_present = total_cont
+    @total_cont = 0
+    add_boats_method
+    @job.cont_present = @total_cont
     if @job.save
       redirect_to profiles_show_path
     else
@@ -43,7 +36,20 @@ class JobsController < ApplicationController
   end
 
   def update
-    total_cont = @job.cont_present
+    @total_cont = @job.cont_present
+    add_boats_method
+    @job.update(job_params)
+    @job.update(cont_present: @total_cont)
+    # redirect_to "edit_job_path"
+  end
+
+  def destroy
+    Job.find(params[:id]).destroy
+    flash[:success] = "order deleted"
+    redirect_to profiles_show_path
+  end
+
+  def add_boats_method
     boatsall = Boat.all
     boatsjob = @job.boats
     @boatsavail = boatsall - boatsjob
@@ -53,19 +59,10 @@ class JobsController < ApplicationController
        j = boat.id
        if (params["#{j}"]["id"] == "1")
          @boat = Boat.find(j)
-         total_cont += @boat.capacity
+         @total_cont += @boat.capacity
          @job.boats.push(@boat)
        end
     end
-    @job.update(job_params)
-    @job.update(cont_present: total_cont)
-    # redirect_to "edit_job_path"
-  end
-
-  def destroy
-    Job.find(params[:id]).destroy
-    flash[:success] = "order deleted"
-    redirect_to profiles_show_path
   end
 
   private
